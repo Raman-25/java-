c0e1e97640b267f68d82398ec9d12c16b647edba
Ramandeep Singh <ramany5979@gmail.com>
2025-08-25 16:53:15 +0530

    Add files via upload

--- Recursion/q8.java
+++ Recursion/q8.java
@@ -1,3 +1,13 @@
 public class q8 {
-    static int avg(int... VarArgs)
+    static float avg(float... arr){//ARRAY IS FORMED
+        float sum = 0;
+        for(float a:arr){
+            sum=sum+a;
+        }
+        return sum/arr.length;
+    }
+
+    static void main() {
+        System.out.println(avg(10,46,78,89,90,67,89,56,35,4665));
+    }
 }
